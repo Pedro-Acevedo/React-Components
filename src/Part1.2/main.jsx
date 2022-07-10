4c1c2d66749ec2191b8b30223936cea3c5a27e09
Pedro <thepeterace@gmail.com>
2022-07-10 13:24:05 -0400

    Pasando props desde un objeto literal

--- src/Part1.2/main.jsx
+++ src/Part1.2/main.jsx
@@ -1,42 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const Header = (props) => {
-    return (<h1> {props.course}</h1>) 
-  }
-const Part =(props) =>{
-  return <p> {props.part} {props.exercises}</p>
-}
-  const Content = (props) => {
-      return (<>
-      <Part part={props.part1} exercises={props.exercises1}></Part>
-      <Part part={props.part2} exercises={props.exercises2}></Part>
-      <Part part={props.part3} exercises={props.exercises3}></Part>
-    </>)
-    }
-  const Total = (props) => {
-      return <>
-        <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3 }
-        </p> 
-      </>
-      }
-
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {name: 'Half Stack application development',
+   parts: [ 
+    { 
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    } 
+  ]
+}
+
+  const Header = ( {course} ) => {
+
+    // const nom = course.name
+    return <h1> { course.name } </h1>
+}
+  const Content = ( {course}  ) => {
+    return <>
+      <p> { course.parts[0].name } { course.parts[0].exercises }</p>
+      <p> { course.parts[1].name } { course.parts[1].exercises }</p>
+      <p> { course.parts[2].name } { course.parts[2].exercises }</p>
+    </>
+  }
+  const Total = ( {course} ) =>{
+  
+    return <>
+    <p> The total is: {course.parts[0].exercises+ course.parts[1].exercises + course.parts[2].exercises}</p>
+    </>
+    return 
+  }
 
   return (
-    <div>
-      <Header course={course}></Header>
-      <Content part1={part1} part2={part2} part3={part3}
-      exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}></Content>
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}></Total>
-    </div>
+    <>
+    <Header course = {course}></Header>
+    <Content course = { course }></Content>
+    <Total course = {course}></Total>
+      {/* <div>
+        { parts[0].name } { parts[0].exercises } 
+      </div> */}
+      {/* <div>
+        { parts[1].name } { parts[1].exercises } 
+      </div>
+      <div>
+        { parts[2].name } { parts[2].exercises } 
+      </div> */}
+      
+    </>
   )
 }
 
